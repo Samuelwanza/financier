9ed3913ae14b0eb719c789b0acb05a4eb64bf4a0
Samuel wanza <s.wanza@alustudent.com>
2024-01-13 09:56:35 +0200

    added devise for production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -94,4 +94,5 @@ Rails.application.configure do
   # ]
   # Skip DNS rebinding protection for the default health check endpoint.
   # config.host_authorization = { exclude: ->(request) { request.path == "/up" } }
+  config.action_mailer.default_url_options = { host: 'https://financier-g2h3.onrender.com/', port: 3000 }
 end
